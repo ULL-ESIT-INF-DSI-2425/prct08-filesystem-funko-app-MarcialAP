dda762b55635a3f781a77fef8447207f084446d0
Marcial Alvarez <alu0100996043@ull.edu.es>
2025-03-24 20:05:00 +0000

    Primera aproximacion

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * Suma dos numeros
+ * @param firstnumber - El primer operando de la suma
+ * @param secondnumber - El segundo operando de la suma
+ * @returns El resultado de la suma
+ * ```typescript
+ * add(1, 7) = 8
+ * add(8, -3) = 5
+ * ```
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = add;
+function add(firstnumber, secondnumber) {
+    return firstnumber + secondnumber;
+}
